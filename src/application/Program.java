409953356ff1e297501d4f9d56f9a062bf174a3f
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2026-01-12 09:33:04 -0300

    criação da classe Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
+
+		Position pos = new Position(0, 0);
+
+		System.out.println(pos);
 		
-		System.out.println("Isso eventualmente vai ser um jogo de xadrez!");
 	}
 
 }

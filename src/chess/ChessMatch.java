9f9ac61a0f6296fae307c239e7d0386e4d935374
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2026-01-13 11:21:31 -0300

    Criação e implementação de Exceções

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,7 +26,7 @@ public class ChessMatch {
 
     private void initialSetup(){
         board.placePiece(new Rook(board, Color.BLACK), new Position(0,7));
-        board.placePiece(new King(board, Color.BLACK), new Position(7,4));
+        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
     }
 
 }
